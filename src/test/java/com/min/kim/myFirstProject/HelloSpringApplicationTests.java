de8e284e39973d3b157c95d75dc5335f07a94dde
minseok <minseok9128@gmail.com>
2022-04-12 01:24:44 +0900

    Initial commit

---
+++ src/test/java/com/min/kim/myFirstProject/HelloSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.min.kim.myFirstProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
