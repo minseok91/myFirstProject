1006a54075bb4801c3839353384697e5c0af8ced
minseok <minseok9128@gmail.com>
2022-04-13 01:12:23 +0900

    김영한 강의 섹션 2 완료

---
+++ src/main/java/com/min/kim/myFirstProject/domain/Member.java
@@ -0,0 +1,19 @@
+package com.min.kim.myFirstProject.domain;
+
+public class Member {
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
