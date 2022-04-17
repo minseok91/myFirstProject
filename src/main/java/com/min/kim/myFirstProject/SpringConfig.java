487cc809435aca8f29c9d2b43f9e657efde15382
minseok <minseok9128@gmail.com>
2022-04-17 13:20:59 +0900

    김영한 강의 섹션 5 스프링 DB 접근 기술 완료 1. H2 데이터베이스 설치 2. 순수 Jdbc 3. 스프링 통합 테스트 스프링 4. JdbcTemplate 5. JPA 6. 스프링 데이터 JPA
    지렸다 JPA
    여기서는 db 접근기술에 대해 배움

--- src/main/java/com/min/kim/myFirstProject/SpringConfig.java
+++ src/main/java/com/min/kim/myFirstProject/SpringConfig.java
@@ -1,22 +1,50 @@
 package com.min.kim.myFirstProject;
 
-import com.min.kim.myFirstProject.repository.MemberRepository;
-import com.min.kim.myFirstProject.repository.MemoryMemberRepository;
+import com.min.kim.myFirstProject.domain.Member;
+import com.min.kim.myFirstProject.repository.*;
 import com.min.kim.myFirstProject.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
-    @Bean
-    public MemberService memberService() {
-        return new MemberService(memberRepository());
+    //private DataSource dataSource;
+
+//    @Autowired
+//    public SpringConfig(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
+
+//    private EntityManager em;
+//
+//    @Autowired
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
-    public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
     }
+
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        //return new MemoryMemberRepository();
+//        //return new JdbcMemberRepository(dataSource);
+//        //return new JdbcTemplateMemberRepository(dataSource);
+//        //return new JpaMemberRepository(em);
+//    }
 
 }
---
+++ src/main/java/com/min/kim/myFirstProject/repository/SpringDataJpaMemberRepository.java
@@ -0,0 +1,12 @@
+package com.min.kim.myFirstProject.repository;
+
+import com.min.kim.myFirstProject.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+
+    @Override
+    Optional<Member> findByName(String name);
+}
